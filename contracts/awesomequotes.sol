64b1a3bc2e779ee3137e8bb7490ee44fb4236aea
Muhindo Galien <74584848+Muhindo-Galien@users.noreply.github.com>
2023-08-30 14:44:22 +0300

    awesomequotes smart contract updated

--- contracts/awesomequotes.sol
+++ contracts/awesomequotes.sol
@@ -2,6 +2,11 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
+/**
+ * @title ERC20 Token Interface
+ * @dev This interface defines the standard functions for ERC20 tokens.
+ */
+
 interface ERC20Token {
   function transfer(address, uint256) external returns (bool);
   function approve(address, uint256) external returns (bool);
@@ -14,10 +19,22 @@ interface ERC20Token {
   event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
+/**
+ * @title AwesomeQuotes Contract
+ * @dev This contract manages quotes, allowing creation, retrieval, and tipping.
+ */
 contract AwesomeQuotes {
     uint internal quotesLength = 0;
     address internal cUSD = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
 
+    /**
+     * @dev Struct to store quote information.
+     * @param owner Address of the quote owner.
+     * @param author Author of the quote.
+     * @param body Text body of the quote.
+     * @param tips Number of tips received for the quote.
+     * @param createdAt Creation timestamp of the quote.
+     */
     struct Quote {
         address payable owner;
         string author;
@@ -26,13 +43,51 @@ contract AwesomeQuotes {
         uint createdAt;
     }
 
+    /**
+     * @dev Mapping to store quotes by index.
+     * The key is the index of the quote, and the value is a struct containing quote details.
+     */
     mapping (uint => Quote) internal quotes;
 
+    /**
+     * @dev Event emitted when a new quote is created.
+     * @param owner The address of the quote owner.
+     * @param author The author of the quote.
+     * @param body The text body of the quote.
+     * @param tips The number of tips received for the quote.
+     * @param createdAt The creation timestamp of the quote.
+     */
+    event QuoteCreated(
+        address indexed owner,
+        string author,
+        string body,
+        uint tips,
+        uint createdAt
+    );
+
+    event QuoteTipped(
+        address indexed giver,
+        uint tip
+    );
+
+
+    /**
+     * @notice Creates a new quote with the given information.
+     * @dev The function creates a new quote with the provided author and body,
+     *      and associates it with the calling address. The creation timestamp
+     *      is set to the current block's timestamp.
+     * @param _createdAt Creation timestamp of the quote.
+     * @param _author Author of the quote.
+     * @param _body Text body of the quote.
+     */
     function createQuote (
         uint _createdAt,
         string memory _author,
         string memory _body
     )   public {
+        require(_createdAt <= block.timestamp, "Invalid creation time");
+        require(bytes(_author).length > 0, "Author cannot be empty");
+        require(bytes(_body).length > 0, "Body cannot be empty");
         uint _tips = 0;
         quotes[quotesLength] = Quote(
             payable (msg.sender),
@@ -42,8 +97,23 @@ contract AwesomeQuotes {
             _createdAt
         );
         quotesLength++;
+
+        emit QuoteCreated(
+            msg.sender,
+            _author,
+            _body,
+            _tips,
+            _createdAt
+        );
     }
 
+    /**
+     * @notice Retrieves the details of a quote at the given index.
+     * @dev This function returns the owner, author, body, tips, and creation timestamp
+     *      of the quote at the specified index.
+     * @param _index Index of the quote.
+     * @return Quote details: owner, author, body, tips, createdAt.
+     */
     function getQuote (uint _index) public view returns 
     (address payable, string memory, string memory, uint, uint ) 
     {
@@ -56,16 +126,33 @@ contract AwesomeQuotes {
         );
     }
 
-       function tipQuote(uint _index, uint _tip) public payable {
+    /**
+     * @notice Tips a quote with the given index by transferring tokens.
+     * @dev This function allows the caller to tip a quote's owner with the specified amount of tokens.
+     *      It transfers tokens from the caller to the quote owner.
+     * @param _index Index of the quote to be tipped.
+     * @param _tip Amount of tokens to be tipped.
+     */
+    function tipQuote(uint _index, uint _tip) public payable {
+        require(_tip > 0 ether, "Tip can not be 0");
+        quotes[_index].tips++;
         require(ERC20Token(cUSD).transferFrom(
             msg.sender,
             quotes[_index].owner,
-            _tip
-
+            _tip    
         ), "Transfer Failed");
-        quotes[_index].tips++;
+
+        emit QuoteTipped(
+            msg.sender,
+            _tip
+        );
     }
 
+     /**
+     * @notice Gets the total number of quotes stored.
+     * @dev This function returns the current number of quotes stored in the contract.
+     * @return Total number of quotes.
+     */
     function getQuoteLength () public view returns (uint) {
         return quotesLength;
     }
